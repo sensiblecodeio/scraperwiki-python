9bf9ae5c13a2894cbead4932a43f4905f5e13b74
Thomas Levine <thomas.levine@gmail.com>
2012-04-26 10:36:54 -0400

    aoeu

--- __init__.py
+++ __init__.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python2
+# Thomas Levine, ScraperWiki Limited
+
+'''
+Local version of ScraperWiki Utils, documentation here:
+https://scraperwiki.com/docs/python/python_help_documentation/
+'''
 
 logfd = None # This does something on the hosted version
 
