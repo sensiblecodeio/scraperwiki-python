e0326f7069d2d09fd514a056c831fb4836998c6a
Thomas Levine <occurrence@thomaslevine.com>
2012-05-23 20:20:40 -0400

    restructure for tests

--- tests.py
+++ tests.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python2
 from unittest import TestCase, main
 from json import loads, dumps
-import sqlite
 import sqlite3
 import os
 import shutil
 import datetime
-from dumptruck.convert import quote
+
+import scraperwiki
+
+# This library
 
 class TestDb(TestCase):
   DBNAME = 'scraperwiki.db'
   def setUp(self):
     self.cleanUp()
-    sqlite._connect(self.DBNAME)
+    scraperwiki.sqlite._connect(self.DBNAME)
 
   def tearDown(self):
     self.cleanUp()
@@ -27,8 +29,8 @@ class TestDb(TestCase):
 
 class SaveGetVar(TestDb):
   def savegetvar(self, var):
-    sqlite.save_var("weird", var)
-    self.assertEqual(sqlite.get_var("weird"), var)
+    scraperwiki.sqlite.save_var("weird", var)
+    self.assertEqual(scraperwiki.sqlite.get_var("weird"), var)
 
 class TestSaveGetList(SaveGetVar):
   def test_list(self):
@@ -41,7 +43,7 @@ class TestSaveGetDict(SaveGetVar):
 class TestSaveVar(TestDb):
   def setUp(self):
     super(TestSaveVar, self).setUp()
-    sqlite.save_var("birthday","November 30, 1888")
+    scraperwiki.sqlite.save_var("birthday","November 30, 1888")
     connection=sqlite3.connect(self.DBNAME)
     self.cursor=connection.cursor()
 
@@ -53,25 +55,25 @@ class TestSaveVar(TestDb):
 
 class TestSelect(TestDb):
   def test_select(self):
-    shutil.copy('dumptruck/fixtures/landbank_branches.sqlite',self.DBNAME)
-    sqlite._connect(self.DBNAME)
-    data_observed = sqlite.select("* FROM `branches` WHERE Fax is not null ORDER BY Fax LIMIT 3;")
+    shutil.copy('fixtures/landbank_branches.sqlite',self.DBNAME)
+    scraperwiki.sqlite._connect(self.DBNAME)
+    data_observed = scraperwiki.sqlite.select("* FROM `branches` WHERE Fax is not null ORDER BY Fax LIMIT 3;")
     data_expected = [{'town': u'\r\nCenturion', 'date_scraped': 1327791915.618461, 'Fax': u' (012) 312 3647', 'Tel': u' (012) 686 0500', 'address_raw': u'\r\n420 Witch Hazel Ave\n\r\nEcopark\n\r\nCenturion\n\r\n0001\n (012) 686 0500\n (012) 312 3647', 'blockId': 14, 'street-address': None, 'postcode': u'\r\n0001', 'address': u'\r\n420 Witch Hazel Ave\n\r\nEcopark\n\r\nCenturion\n\r\n0001', 'branchName': u'Head Office'}, {'town': u'\r\nCenturion', 'date_scraped': 1327792245.787187, 'Fax': u' (012) 312 3647', 'Tel': u' (012) 686 0500', 'address_raw': u'\r\n420 Witch Hazel Ave\n\r\nEcopark\n\r\nCenturion\n\r\n0001\n (012) 686 0500\n (012) 312 3647', 'blockId': 14, 'street-address': u'\r\n420 Witch Hazel Ave\n\r\nEcopark', 'postcode': u'\r\n0001', 'address': u'\r\n420 Witch Hazel Ave\n\r\nEcopark\n\r\nCenturion\n\r\n0001', 'branchName': u'Head Office'}, {'town': u'\r\nMiddelburg', 'date_scraped': 1327791915.618461, 'Fax': u' (013) 282 6558', 'Tel': u' (013) 283 3500', 'address_raw': u'\r\n184 Jan van Riebeeck Street\n\r\nMiddelburg\n\r\n1050\n (013) 283 3500\n (013) 282 6558', 'blockId': 17, 'street-address': None, 'postcode': u'\r\n1050', 'address': u'\r\n184 Jan van Riebeeck Street\n\r\nMiddelburg\n\r\n1050', 'branchName': u'Middelburg'}]
     self.assertListEqual(data_observed, data_expected)
 
 class TestShowTablesIterator(TestDb):
   def test_show_tables(self):
-    shutil.copy('dumptruck/fixtures/landbank_branches.sqlite',self.DBNAME)
-    sqlite._connect(self.DBNAME)
-    observed = set([name for name in sqlite.show_tables()])
+    shutil.copy('fixtures/landbank_branches.sqlite',self.DBNAME)
+    scraperwiki.sqlite._connect(self.DBNAME)
+    observed = set([name for name in scraperwiki.sqlite.show_tables()])
     expected = set(['blocks','branches'])
     self.assertSetEqual(observed, expected)
 
 class TestShowTablesDict(TestDb):
   def test_show_tables(self):
-    shutil.copy('dumptruck/fixtures/landbank_branches.sqlite', self.DBNAME)
-    sqlite._connect(self.DBNAME)
-    observed = sqlite.show_tables()
+    shutil.copy('fixtures/landbank_branches.sqlite', self.DBNAME)
+    scraperwiki.sqlite._connect(self.DBNAME)
+    observed = scraperwiki.sqlite.show_tables()
     expected = {
       'blocks': 'CREATE TABLE `blocks` (`blockPerson` text, `date_scraped` real, `region` text, `blockId` integer, `blockName` text)',
       'branches': 'CREATE TABLE `branches` (`town` text, `date_scraped` real, `Fax` text, `Tel` text, `address_raw` text, `blockId` integer, `postcode` text, `address` text, `branchName` text, `street-address` text)'
@@ -81,10 +83,10 @@ class TestShowTablesDict(TestDb):
 class SaveAndCheck(TestDb):
   def save_and_check(self, dataIn, tableIn, dataOut, tableOut = None, twice = True):
     if tableOut == None:
-      tableOut = quote(tableIn)
+      tableOut = '[' + tableIn + ']'
 
     # Insert
-    sqlite.save([], dataIn, tableIn)
+    scraperwiki.sqlite.save([], dataIn, tableIn)
 
     # Observe with pysqlite
     connection=sqlite3.connect(self.DBNAME)
@@ -95,7 +97,7 @@ class SaveAndCheck(TestDb):
 
     if twice:
       # Observe with DumpTruck
-      observed2 = sqlite.select('* FROM %s' % tableOut)
+      observed2 = scraperwiki.sqlite.select('* FROM %s' % tableOut)
  
       #Check
       expected1 = dataOut
@@ -104,46 +106,16 @@ class SaveAndCheck(TestDb):
       self.assertListEqual(observed1, expected1)
       self.assertListEqual(observed2, expected2)
       
-
-class TestSaveDict(SaveAndCheck):
-  def test_save_integers(self):
-    d = {1: "A", 2: "B", 3: "C"}
-    self.assertRaises(TypeError, lambda: self.save_and_check(
-      {"modelNumber": d}
-    , "model-numbers"
-    , [(dumps(d),)]
-    ))
-
-  def test_save_text(self):
-    d = {'1': 'A', '2': 'B', '3': 'C'}
-    self.save_and_check(
-      {"modelNumber": d}
-    , "model-numbers"
-    , [(dumps(d),)]
-    )
-
-  def test_save_fanciness(self):
-    d = {'1': datetime.datetime(2012, 3, 5)}
-    self.assertRaises(TypeError, lambda: self.save_and_check(
-      {"modelNumber": d}
-    , "model-numbers"
-    , [(dumps(d),)]
-    ))
-
 class SaveAndSelect(TestDb):
   def save_and_select(self, d):
-    sqlite.save([], {"foo": d})
+    scraperwiki.sqlite.save([], {"foo": d})
 
-    observed = sqlite.select('* from swdata')[0]['foo']
+    observed = scraperwiki.sqlite.select('* from swdata')[0]['foo']
     self.assertEqual(d, observed)
 
 #class TestSaveLambda(SaveAndSelect):
 #  def test_save_lambda(self):
 #    self.save_and_select(lambda x: x^2)
-
-class TestSaveSet(SaveAndSelect):
-  def test_save_set(self):
-    self.save_and_select(set(["A", "B", "C"]))
 
 class TestSaveBoolean(SaveAndCheck):
   def test_save_true(self):
@@ -158,15 +130,6 @@ class TestSaveBoolean(SaveAndCheck):
       {"a": False}
     , "a"
     , [(0,)]
-    )
-
-class TestSaveList(SaveAndCheck):
-  def test_save_integers(self):
-    d = ["A", "B", "C"]
-    self.save_and_check(
-      {"model-codes": d}
-    , "models"
-    , [(dumps(d),)]
     )
 
 class TestSaveTwice(SaveAndCheck):
@@ -191,29 +154,6 @@ class TestSaveInt(SaveAndCheck):
     , [(293,)]
     )
 
-class TestSaveWeirdTableName1(SaveAndCheck):
-  def test_save(self):
-    self.save_and_check(
-      {"modelNumber": 293}
-    , "This should-be a_valid.table+name!?"
-    , [(293,)]
-    )
-
-class TestSaveWeirdTableName2(SaveAndCheck):
-  def test_save(self):
-    self.save_and_check(
-      {"lastname":"LeTourneau"}
-    , "`asoeu`"
-    , [(u'LeTourneau',)]
-    )
-
-class TestSaveWeirdTableName3(SaveAndCheck):
-  def test_save(self):
-    self.save_and_check(
-      {"lastname":"LeTourneau"}
-    , "[asoeu]"
-    , [(u'LeTourneau',)]
-    )
 
 class TestMultipleColumns(SaveAndSelect):
   def test_save(self):
@@ -254,8 +194,8 @@ class TestSaveDateTime(SaveAndCheck):
 class TestAttach(TestDb):
   def hsph(self):
     #https://scraperwiki.com/scrapers/export_sqlite/hsph_faculty_1/
-    sqlite.attach('hsph_faculty_1')
-    observed = sqlite.select('count(*) as "c" from maincol')[0]['c']
+    scraperwiki.sqlite.attach('hsph_faculty_1')
+    observed = scraperwiki.sqlite.select('count(*) as "c" from maincol')[0]['c']
     expected = 461
     self.assertEqual(observed, expected)
 
@@ -278,7 +218,7 @@ class TestAttach(TestDb):
 
 class TestSWImport(TestCase):
   def test_csv2sw(self):
-    csv2sw = swimport('csv2sw')
+    csv2sw = scraperwiki.swimport('csv2sw')
     self.assertEquals(type(csv2sw.read.csv), type(lambda : None) )
 
 if __name__ == '__main__':
