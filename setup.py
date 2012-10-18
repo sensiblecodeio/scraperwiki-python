bfa50c5ad1173d2247f17ea3283e8530a3c26eb7
Francis Irving <francis@mysociety.org>
2012-10-18 10:09:19 +0100

    Using LICENCE file now

--- setup.py
+++ setup.py
@@ -2,19 +2,6 @@
 
 # This file is part of Local Scraperlibs.
 
-# Local Scraperlibs is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# Local Scraperlibs is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Public License for more details.
-
-# You should have received a copy of the GNU Public License
-# along with Local Scraperlibs.  If not, see <http://www.gnu.org/licenses/>.
-
 import warnings
 import os
 from distutils.core import setup
