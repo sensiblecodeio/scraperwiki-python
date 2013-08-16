a2b4ef334a75951a931adbfa2b8c2eb564f3cf0b
Peter Waller <p@pwaller.net>
2013-08-16 15:15:17 +0100

    Rename _sw_error to _sw_runlog and record successful exits

--- scraperwiki/runlog/__init__.py
+++ scraperwiki/runlog/__init__.py
@@ -1,3 +1,4 @@
+import atexit
 import datetime
 import inspect
 import os
@@ -6,9 +7,14 @@ import traceback
 
 import scraperwiki
 
+_successful_exit = True
+
 _inner_excepthook = sys.excepthook
 def sw_excepthook(type, value, tb):
     """Log uncaught exceptions to scraperwiki.sqlite file."""
+
+    global _successful_exit
+    _successful_exit = False
 
     try:
         first_frame_tuple = inspect.getouterframes(tb.tb_frame)[-1]
@@ -18,14 +24,24 @@ def sw_excepthook(type, value, tb):
 
         message = repr(value)
         
-        d = dict(time=datetime.datetime.now(), path=filename,
-          traceback=''.join(traceback.format_tb(tb)),
-          pwd=os.getcwd(), exception_type=type_name, exception_value=message)
+        d = dict(time=datetime.datetime.now(), path=filename, pwd=os.getcwd(),
+            traceback=''.join(traceback.format_tb(tb)),
+            exception_type=type_name, exception_value=message,
+            success=False)
 
-        scraperwiki.sql.save([], d, table_name="_sw_error")
+        scraperwiki.sql.save([], d, table_name="_sw_runlog")
+
         scraperwiki.status('error')
     finally:
         _inner_excepthook(type, value, tb)
 
 
 sys.excepthook = sw_excepthook
+
+
+@atexit.register
+def successful_exit():
+    if _successful_exit:
+        d = dict(time=datetime.datetime.now(), path=filename, pwd=os.getcwd(),
+            success=True)
+        scraperwiki.sql.save([], d, table_name="_sw_runlog")
