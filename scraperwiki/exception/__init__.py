0983611262d2ad08a4a3660486d22868f2e2a8b8
Peter Waller <p@pwaller.net>
2013-08-16 13:17:30 +0100

    Fix exception hook test to ensure that traceback isn't broken (and fix exception)

--- scraperwiki/exception/__init__.py
+++ scraperwiki/exception/__init__.py
@@ -13,12 +13,16 @@ def sw_excepthook(type, value, tb):
     try:
         first_frame_tuple = inspect.getouterframes(tb.tb_frame)[-1]
         (_frame, filename, _lineno, _where, _code, _) = first_frame_tuple
+        
         type_name = type.__module__ + '.' + type.__name__
+        
         d = dict(time=datetime.datetime.now(), path=filename,
           traceback=''.join(traceback.format_tb(tb)),
           pwd=os.getcwd(), exception_type=type_name, exception_value=repr(value))
+
         scraperwiki.sql.save([], d, table_name="_sw_error")
     finally:
-        _inner_excepthook(type, value, traceback)
+        _inner_excepthook(type, value, tb)
+
 
 sys.excepthook = sw_excepthook
