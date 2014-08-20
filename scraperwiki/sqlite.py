72622a5c1e033c11bbc5c479219440af0c8618c5
Peter Waller <peter@scraperwiki.com>
2014-08-20 17:09:19 +0100

    Add save buffering for 10x speedup

--- scraperwiki/sqlite.py
+++ scraperwiki/sqlite.py
@@ -1,7 +1,13 @@
+from collections import Iterable, Mapping
 from dumptruck import DumpTruck
+
+import atexit
 import datetime
-import re
 import os
+import re
+import time
+import sys
+
 
 DATABASE_NAME = os.environ.get("SCRAPERWIKI_DATABASE_NAME", "scraperwiki.sqlite")
 DATABASE_TIMEOUT = float(os.environ.get("SCRAPERWIKI_DATABASE_TIMEOUT", 300))
@@ -15,8 +21,119 @@ def _connect(dbname=DATABASE_NAME, timeout=DATABASE_TIMEOUT):
 
 _connect()
 
+class _Buffer(object):
+    """
+    Buffer holds all state relating to scraperwiki.sqlite.save.
+
+    The strategy is to buffer at most 50,000 records for at most 20 seconds.
+    """
+    # We measured O(0.5 seconds) between flushes with MAX_SIZE = 1000, so
+    # don't penalize users who aren't hammering the system much more than this.
+    MAX_SECONDS_BEFORE_FLUSH = 3
+
+    # Measured to be a sweet spot with save_speedtest.py
+    MAX_SIZE = 1000
+    BAD_TYPE = "scraperwiki.sql.save requires mapping or iterable of mapping"
+
+    buffered_saves = []
+    buffered_table = None
+    unique_keys = []
+    flushing = False
+
+    # time after which records should be automatically flushed.
+    # It isn't a guarantee, but if the deadline is passed after an append,
+    # then a flush will occur.
+    flush_deadline = time.time() + MAX_SECONDS_BEFORE_FLUSH
+
+    @classmethod
+    def flush(cls):
+        if cls.flushing:
+            raise RuntimeError("Double flush")
+            return
+
+        cls.flushing = True
+
+        if not cls.buffered_saves:
+            cls.flushing = False
+            return
+
+        real_save(cls.unique_keys, cls.buffered_saves, cls.buffered_table)
+
+        cls.unique_keys = None
+        cls.buffered_table = None
+        # In place list update to preserve other references to this list.
+        cls.buffered_saves[:] = []
+
+        cls.flush_deadline = time.time() + cls.MAX_SECONDS_BEFORE_FLUSH
+
+        # It might be tempting to put this in a try-finally, but we *really*
+        # aren't done until the above code has run.
+        cls.flushing = False
+
+    @classmethod
+    def append(cls, unique_keys, data, table_name):
+        if cls.unique_keys != unique_keys or cls.buffered_table != table_name:
+            cls.flush()
+        cls.unique_keys = unique_keys
+        cls.buffered_table = table_name
+
+        if isinstance(data, Mapping):
+            cls.buffered_saves.append(data)
+            if len(cls.buffered_saves) >= cls.MAX_SIZE:
+                cls.flush()
+
+        elif isinstance(data, Iterable):
+            # Cache lookups
+            buffered_saves = cls.buffered_saves
+            append = cls.buffered_saves.append
+            MAX_SIZE = cls.MAX_SIZE
+
+            for datum in data:
+                if not isinstance(datum, Mapping):
+                    raise TypeError(cls.BAD_TYPE, type(datum))
+                append(datum)
+                if len(buffered_saves) >= MAX_SIZE:
+                    cls.flush()
+        else:
+            # Not mapping or iterable of mapping
+            raise TypeError(cls.BAD_TYPE, type(data))
+
+        if cls.flush_deadline < time.time():
+            # print "Flush deadline passed"
+            cls.flush()
+
+def flush():
+    """
+    Ensure that any buffered records are written out to sqlite
+    """
+    _Buffer.flush()
+
+@atexit.register
+def _finish():
+    """
+    Atexit handler to empty the buffer at process end
+    """
+    # print "Final buffer flush."
+
+    if _Buffer.flushing:
+        # The only way flushing can be true is if exit() is called during
+        # flush(), in which case flushing won't work.
+        return
+
+    flush()
+
+_ORIG_EXCEPTHOOK = sys.excepthook
+def _excepthook(*args, **kwargs):
+    # print "Flushing due to exception"
+    if not _Buffer.flushing:
+        flush()
+    return _ORIG_EXCEPTHOOK(*args, **kwargs)
+sys.excepthook = _excepthook
+
 def execute(sqlquery, data=[], verbose=1):
     """ Emulate scraperwiki as much as possible by mangling dumptruck result """
+    flush()
+
     # Allow for a non-list to be passed as data.
     if type(data) != list and type(data) != tuple:
         data = [data]
@@ -33,15 +150,23 @@ def execute(sqlquery, data=[], verbose=1):
 def save(unique_keys, data, table_name="swdata", verbose=2, date=None):
     if not data:
         return
+    _Buffer.append(unique_keys, data, table_name)
+
+def real_save(unique_keys, data, table_name="swdata", verbose=2, date=None):
+    # print "Real save called, len(data) = {}".format(len(data))
+    if not data:
+        return
     dt.create_table(data, table_name = table_name, error_if_exists = False)
     if unique_keys != []:
         dt.create_index(unique_keys, table_name, unique = True, if_not_exists = True)
     return dt.upsert(data, table_name = table_name)
 
 def commit(verbose=1):
+    flush()
     dt.commit()
 
 def select(sqlquery, data=[], verbose=1):
+    flush()
     sqlquery = "select %s" % sqlquery   # maybe check if select or another command is there already?
     result = dt.execute(sqlquery, data, commit = False)
     # Convert dates to strings to conform to scraperwiki classic
@@ -54,6 +179,7 @@ def select(sqlquery, data=[], verbose=1):
     return result
 
 def show_tables(dbname=""):
+    flush()
     name = "sqlite_master"
     if dbname:
         name = "`%s`.%s" % (dbname, name)
@@ -61,6 +187,7 @@ def show_tables(dbname=""):
     return {row['name']: row['sql'] for row in response}
 
 def save_var(name, value, verbose=2):
+    flush()
     data = dt.save_var(name, value)
     dt.execute(u"CREATE TABLE IF NOT EXISTS swvariables (`value_blob` blob, `type` text, `name` text PRIMARY KEY)", commit = False)
     dt.execute(u'INSERT OR REPLACE INTO swvariables SELECT `value`, `type`, `key` FROM `%s`' % dt._DumpTruck__vars_table, commit = False)
@@ -69,6 +196,7 @@ def save_var(name, value, verbose=2):
     return data
 
 def get_var(name, default=None, verbose=2):
+    flush()
     if 'swvariables' not in show_tables(): # this should be unecessary
         return default
     dt.execute(u"CREATE TABLE IF NOT EXISTS swvariables (`value_blob` blob, `type` text, `name` text PRIMARY KEY)", commit = False)
