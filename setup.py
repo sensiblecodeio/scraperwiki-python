8f5227daf1ea20f34b900c588b54c748f373fc73
Peter Waller <peter@scraperwiki.com>
2014-09-05 17:08:29 +0100

    Depend on sqlalchemy and alembic

--- setup.py
+++ setup.py
@@ -36,14 +36,14 @@ config = dict(name='scraperwiki',
               'Topic :: Database :: Front-Ends',
               ],
               packages=['scraperwiki', 'scraperwiki.runlog'],
-
               version='0.3.11',
               license='GPL',
               )
 
 try:
     from setuptools import setup
-    config['install_requires'] = ['dumptruck>=0.1.6', 'requests'],
+    config['install_requires'] = ['dumptruck>=0.1.6', 'requests',
+                                  'sqlalchemy', 'alembic'],
 except ImportError:
     pass
 
