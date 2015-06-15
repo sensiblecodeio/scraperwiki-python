53553ab287a1ffdf6542353a37a5074047710f9f
Dragon Dave McKee <dragon@scraperwiki.com>
2015-06-15 10:41:54 +0100

    bump version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ config = dict(name='scraperwiki',
               'Topic :: Database :: Front-Ends',
               ],
               packages=['scraperwiki'],
-              version='0.4.1',
+              version='0.4.2',
               license='GPL',
               )
 
