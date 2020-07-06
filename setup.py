ceda390d41b0b82aebbf3431bc08fa6da08ec43c
Steven Maude <git@stevenmaude.co.uk>
2020-07-06 23:17:11 +0100

    Try older sqlalchemy version
    Back when tests passed.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ config = dict(name='scraperwiki',
 try:
     from setuptools import setup
     config['install_requires'] = ['requests', 'six',
-                                  'sqlalchemy', 'alembic'],
+                                  'sqlalchemy==1.0.19', 'alembic'],
 except ImportError:
     pass
 
